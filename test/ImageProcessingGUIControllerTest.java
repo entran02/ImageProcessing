c4fd167b8e6bb9490d99a22942c78fec22db32fd
entran02 <evanntran2002@gmail.com>
2022-11-22 20:40:44 -0500

    action and slider tests

--- test/ImageProcessingGUIControllerTest.java
+++ test/ImageProcessingGUIControllerTest.java
@@ -3,6 +3,9 @@ import org.junit.Test;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+
 import controller.ImageProcessingGUIController;
 import model.ImageProcessingModel;
 import model.ImageProcessingModelImpl;
@@ -52,4 +55,181 @@ public class ImageProcessingGUIControllerTest {
     assertEquals(expectedOut, log.toString());
   }
 
+  @Test
+  public void testGreenComponent() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "green-component"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testBlueComponent() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "blue-component"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testBlur() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "blur"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testSharpen() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "sharpen"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testHorizontal() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "horizontal-flip"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testVertical() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "vertical-flip"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testGreyscale() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "greyscale"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testIntensityComponent() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "intensity-component"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testLumaComponent() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "luma-component"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testValueComponent() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "value-component"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testSepia() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "sepia"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testBrightness() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.stateChanged(new ChangeEvent(new JSlider()));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
+
+  @Test
+  public void testMultipleActions() {
+    this.model = new ImageProcessingModelImpl();
+    model.add("Image", TestingUtil.getKirby());
+    this.log = new StringBuilder();
+    this.view = new ViewMock(log);
+    this.controller = new ImageProcessingGUIController(model, view);
+    controller.actionPerformed(new ActionEvent("", 0, "sepia"));
+    controller.actionPerformed(new ActionEvent("", 0, "horizontal-flip"));
+    controller.stateChanged(new ChangeEvent(new JSlider()));
+    controller.actionPerformed(new ActionEvent("", 0, "vertical-flip"));
+    String expectedOut = "Image displayed\n" +
+            "Histogram displayed\n" +
+            "Image displayed\n" +
+            "Histogram displayed\n" +
+            "Image displayed\n" +
+            "Histogram displayed\n" +
+            "Image displayed\n" +
+            "Histogram displayed\n";
+    assertEquals(expectedOut, log.toString());
+  }
 }
